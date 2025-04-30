48f5ab5d4a7435bae04fbe5eeeab351f64497ab4
jaycode8 <jamesmumo443@gmail.com>
2025-04-30 13:05:01 +0300

    feat(todo) added CRUD op for tasks

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # owner = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "todo"
+        verbose_name = "todo"
+        verbose_name_plural = "todos"
+
+    def __str__(self):
+        return self.title
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+class TodoViewSet(ModelViewSet):
+    queryset = Todo.objects.all().order_by("-created_at")
+    serializer_class = TodoSerializer
