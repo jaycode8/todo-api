118dae29fc55a66279701fb079b98994250914db
Oliver Mutuku <olivermulei001.com>
2025-05-05 11:21:44 +0300

    Rename default DB file to more descriptive name (todo_project.sqlite3)

--- todo/settings.py
+++ todo/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'todo_project.sqlite3',
     }
 }
 
